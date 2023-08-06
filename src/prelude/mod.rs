f519d261a5e093339a4a7df7fbcfd7c27dfe23bf
Pxdr0-A <113845824+Pxdr0-A@users.noreply.github.com>
2023-08-06 17:42:30 +0100

    Update mod.rs

--- src/prelude/mod.rs
+++ src/prelude/mod.rs
@@ -1,11 +1,11 @@
-pub fn lcg(seed: u128) -> (u128, f64) {
+pub fn lcg(seed: &mut u128) -> f64 {
     // IBM C/C++ convention params
     let a: u128 = 1103515245;
     let b: u128 = 12345;
     let m: u128 = 2u128.pow(31);
-    
-    let rand_num = (a*seed + b) %  (m - 1);
-    let rand = (rand_num as f64)/(m as f64);
-    
-    (rand_num, rand)
+
+    *seed = (a * *seed + b) % (m - 1);
+    let rand = (*seed as f64) / (m as f64);
+
+    rand
 }
