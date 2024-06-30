97338fe76243b29aa6f8d0a7730516c3ec5aa75a
Pedro Alves <pedromfa98@gmail.com>
2024-06-30 08:56:20 +0100

    final update on random lcg - it was tested and used extensibly in the renplex library

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,1 @@
 pub mod prelude;
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn lcg_example() {
-        let mut seed = 12345u128;
-        let mut val: f64;
-        for _ in 0..10 {
-            val = prelude::lcg(&mut seed);
-            println!("Value Update: {}", val);
-            println!("Seed Update: {}", seed);
-        }
-    }
-}
